6416c4a71fcc1c062b4a7ff1cfec1b9f159468f2
standbymarcenaria-crypto <standbymarcenaria@gmail.com>
2026-06-15 14:42:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // MODO LOCAL: deixe vazio para testar sem Supabase.
 // Depois que criar o Supabase, preencha com os dados de Project Settings > API.
 window.LOGFER_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://ojxatyafgfesswpvuqnz.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeGF0eWFmZ2Zlc3N3cHZ1cW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MzE4MjcsImV4cCI6MjA5NzEwNzgyN30.rPJfRB-kPdHLJ5c76tmyr6qumeu-hYkBnN7UZJO8zRU"
 };
